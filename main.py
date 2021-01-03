6b5edb96422a76df88255669280bdc2b9949be8e
Mustafa Tai <muhammadaslam1292@gmail.com>
2021-01-02 21:32:53 -0600

    added jump, background and character (downloaded from github)
    optimization needed

--- main.py
+++ main.py
@@ -1,58 +1,93 @@
 import pygame
 
-#1 initiallize
+
 pygame.init()
 
-#2the game window
-win = pygame.display.set_mode((500,500))
 
-#game caption
+win = pygame.display.set_mode((500,480))
+
+
 pygame.display.set_caption("My game")
 
-#3 character attributes
+walkRight = [pygame.image.load('game/R1.png'), pygame.image.load('game/R2.png'), pygame.image.load('game/R3.png'), pygame.image.load('game/R4.png'), pygame.image.load('game/R5.png'), pygame.image.load('game/R6.png'), pygame.image.load('game/R7.png'), pygame.image.load('game/R8.png'), pygame.image.load('game/R9.png')]
+walkLeft = [pygame.image.load('game/L1.png'), pygame.image.load('game/L2.png'), pygame.image.load('game/L3.png'), pygame.image.load('game/L4.png'), pygame.image.load('game/L5.png'), pygame.image.load('game/L6.png'), pygame.image.load('game/L7.png'), pygame.image.load('game/L8.png'), pygame.image.load('game/L9.png')]
+bg = pygame.image.load('game/bg.jpg')
+char = pygame.image.load('game/standing.png')
 
-#character position    
+clock = pygame.time.Clock()
 x = 50 
-y = 50
+y = 400
 
-#character make
-width = 40
-height = 50
-vel = 5 #velocity
+width = 64
+height = 64
+vel = 5 
 screenwidth = 500
-
-isjump = True
+isjump = False
 jumpcount = 10
-#4 main loop where pygame checks for mouse and keyboard events
+left = False
+right = False
+walkcout = 0
+
+def redrawGameWindow():
+    global walkcout
+    win.blit(bg, (0,0))        
+    if walkcout + 1 >= 27:
+        walkcout = 0
+    if left:
+        win.blit(walkLeft[walkcout//3], (x,y))
+        walkcout += 1
+    elif right:
+        win.blit(walkRight[walkcout//3], (x,y))
+        walkcout += 1
+    else: 
+        win.blit(char, (x,y))
+    pygame.display.update();
 
 running = True
 while running:
-    pygame.time.delay(100)
-    #check for events (inputs) 
-    for event in pygame.event.get():  #check if exit button is entered
+    clock.tick(27)
+    
+    for event in pygame.event.get():  
         if event.type == pygame.QUIT:
             running = False
 
-        #6 what if a key is held down
-    keys = pygame.key.get_pressed() #coordinates change with the arrow keys
+        
+    keys = pygame.key.get_pressed() 
         
     if keys[pygame.K_LEFT] and x > vel:
         x -= vel
+        left = True
+        right = False
 
-    if keys[pygame.K_RIGHT] and x < screenwidth - width - vel:
+    elif keys[pygame.K_RIGHT] and x < screenwidth - width - vel:
         x += vel
+        right = True
+        left = False
+    else:
+        right = False
+        left = False
+        walkcout = 0
+    if not (isjump):
+        #if keys[pygame.K_UP] and y > vel:
+        #    y -= vel
+                
+        #if keys[pygame.K_DOWN] and y < screenwidth - height - vel:
+        #    y += vel
 
-    if keys[pygame.K_UP] and y > vel:
-        y -= vel
-            
-    if keys[pygame.K_DOWN] and y < screenwidth - height - vel:
-        y += vel
+        if keys[pygame.K_SPACE]:
+            isjump = True
+            right = False
+            left = False
+    else:
+        if jumpcount >= -10:
+            y -= (jumpcount * abs(jumpcount)) * 0.5
+            jumpcount -= 1
+        else: 
+            jumpcount = 10
+            isjump = False
+        
+
+    redrawGameWindow()
 
     
-
-
-    win.fill((0,0,0))
-        
-    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))#5 drawing shapes 1st the surface , 2nd the color (RGB), X Y coordinates, height and width
-    pygame.display.update();
 pygame.quit
